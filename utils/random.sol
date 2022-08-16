9720d4e17752906c5e12539967916a9c2534d8e2
我握我喔我 <8189552+yznyee@user.noreply.gitee.com>
2022-08-16 15:26:21 +0800

    update

--- utils/random.sol
+++ utils/random.sol
@@ -17,268 +17,72 @@ pragma solidity ^0.8.0;
  * NOTE: `SafeMath` is generally not needed starting with Solidity 0.8, since the compiler
  * now has built in overflow checking.
  */
-library SafeMath {
-/**
- * @dev Returns the addition of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-unchecked {
-uint256 c = a + b;
-if (c < a) return (false, 0);
-return (true, c);
-}
-}
 
-/**
- * @dev Returns the subtraction of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-unchecked {
-if (b > a) return (false, 0);
-return (true, a - b);
-}
-}
 
-/**
- * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-unchecked {
-// Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-// benefit is lost if 'b' is also tested.
-// See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-if (a == 0) return (true, 0);
-uint256 c = a * b;
-if (c / a != b) return (false, 0);
-return (true, c);
-}
-}
-
-/**
- * @dev Returns the division of two unsigned integers, with a division by zero flag.
-     *
-     * _Available since v3.4._
-     */
-function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-unchecked {
-if (b == 0) return (false, 0);
-return (true, a / b);
-}
-}
-
-/**
- * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
-     *
-     * _Available since v3.4._
-     */
-function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-unchecked {
-if (b == 0) return (false, 0);
-return (true, a % b);
-}
-}
-
-/**
- * @dev Returns the addition of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `+` operator.
-     *
-     * Requirements:
-     *
-     * - Addition cannot overflow.
-     */
-function add(uint256 a, uint256 b) internal pure returns (uint256) {
-return a + b;
-}
-
-/**
- * @dev Returns the subtraction of two unsigned integers, reverting on
-     * overflow (when the result is negative).
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     *
-     * - Subtraction cannot overflow.
-     */
-function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-return a - b;
-}
-
-/**
- * @dev Returns the multiplication of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `*` operator.
-     *
-     * Requirements:
-     *
-     * - Multiplication cannot overflow.
-     */
-function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-return a * b;
-}
-
-/**
- * @dev Returns the integer division of two unsigned integers, reverting on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator.
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-function div(uint256 a, uint256 b) internal pure returns (uint256) {
-return a / b;
-}
-
-/**
- * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * reverting when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-return a % b;
-}
-
-/**
- * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
-     * overflow (when the result is negative).
-     *
-     * CAUTION: This function is deprecated because it requires allocating memory for the error
-     * message unnecessarily. For custom revert reasons use {trySub}.
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     *
-     * - Subtraction cannot overflow.
-     */
-function sub(
-uint256 a,
-uint256 b,
-string memory errorMessage
-) internal pure returns (uint256) {
-unchecked {
-require(b <= a, errorMessage);
-return a - b;
-}
-}
-
-/**
- * @dev Returns the integer division of two unsigned integers, reverting with custom message on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-function div(
-uint256 a,
-uint256 b,
-string memory errorMessage
-) internal pure returns (uint256) {
-unchecked {
-require(b > 0, errorMessage);
-return a / b;
-}
-}
-
-/**
- * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * reverting with custom message when dividing by zero.
-     *
-     * CAUTION: This function is deprecated because it requires allocating memory for the error
-     * message unnecessarily. For custom revert reasons use {tryMod}.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-function mod(
-uint256 a,
-uint256 b,
-string memory errorMessage
-) internal pure returns (uint256) {
-unchecked {
-require(b > 0, errorMessage);
-return a % b;
-}
-}
-}
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
 contract random is Ownable {
-    uint256 seed;
-    uint256 b;
-    uint356 c;
-    uint256 m;
-    uint level;
+    uint256 _seed;
+    uint256 _b;
+    uint256 _c;
+    uint256 _m;
+    uint _level;
     uint256 cur;
-    constructor(uint256 _seed,uint _level,uint _b,uint _c,uint _m)  {
-        super();
+    event getrand(uint256);
+    constructor(uint256 seed,uint level,uint b,uint c,uint m) Ownable() {
         uint256 pre;
-        level=_level;
-        pre=_seed;
-        b=_b;
-        c=_c;
-        m=_m;
+        _level=level;
+        pre=seed;
+        _b=b;
+        _c=c;
+        _m=m;
         for(uint i=0;i<level;i++) {
+            pre=uint256(keccak256(abi.encodePacked(pre)));
+        }
+        _seed=pre;
+        cur=_seed;
+    }
+    function _setseed(uint256 seed) internal {
+        uint256 pre=seed;
+        for(uint i=0;i<_level;i++) {
             pre=uint256(keccak256(abi.encodePacked(pre)));
         }
         seed=pre;
         cur=seed;
     }
-    function _setseed(uint256 _seed) internal {
-        uint256 pre=_seed;
-        for(uint i=0;i<level;i++) {
-            pre=uint256(keccak256(abi.encodePacked(pre)));
-        }
-        seed=pre;
-        cur=seed;
-        }
-    function _setlevel(uint256 _level) {
-        level=_level;
+    function setseed(uint256 seed) public onlyOwner {
+        _setseed(seed);
     }
-    function _setparam(uint b, uint c, uint m) {
-        b=_b;
-        c=_c;
-        m=_m;
+    function _setlevel(uint256 level) internal {
+        _level=level;
     }
-    function setparam(uint b,uint c, uint m) {
+    function _setparam(uint b, uint c, uint m) internal  {
+        _b=b;
+        _c=c;
+        _m=m;
+    }
+
+
+    function setparam(uint b,uint c, uint m)  public onlyOwner {
         _setparam(b,c,m);
     }
     function _random() internal returns(uint256) {
         uint256 pre=cur;
-        (f,v)=SafeMath.tryMul(cur,b);
+        uint256 v;
+        bool f;
+        (f,v)=SafeMath.tryMul(cur,_b);
         if(!f) {
-            cur=cur%m;
+            cur=cur%_m;
         }
-        cur=(cur*b+c)%m;
+        cur=(cur*_b+_c)%_m;
         return pre;
+    }
+    function rand() public onlyOwner returns(uint256){
+        uint256 v=_random();
+        emit getrand(v);
+        return v;
     }
 
 }
